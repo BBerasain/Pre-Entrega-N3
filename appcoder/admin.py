033ee0fd9012cf85420b7069a4e8c449468474d6
BBerasain <146792109+BBerasain@users.noreply.github.com>
2024-01-04 19:15:08 -0300

    Add files via upload

---
+++ appcoder/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from appcoder.models import Profesor, Curso, Estudiante
+
+admin.site.register(Profesor)
+admin.site.register(Estudiante)
+admin.site.register(Curso)
+
+# Register your models here.

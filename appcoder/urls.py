4786e945953343dd54106c71ee1f6fca914f0573
BBerasain <146792109+BBerasain@users.noreply.github.com>
2024-01-04 19:13:59 -0300

    Delete appcoder directory

--- appcoder/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from appcoder.views import index, Profesores, Estudiantes, Cursos
-urlpatterns = [
-    path("",index, name="Inicio"),
-    path("Profesores/", Profesores, name="profesores"),
-    path("Estudiantes/", Estudiantes, name="Estudiantes"),
-    path("Curso/", Cursos, name="Cursos"),
-]
